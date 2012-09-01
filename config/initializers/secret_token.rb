01854fd856f05eac07ee7a1cfbdc133d2ea94363
pywebdesign <pierre-yves@pywebdesign.net>
2012-09-01 17:16:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '87e40ecbc9e226a7509ca13d18a0ea5dac67c746e1ea416706aec598fdb92452b067a52ce127869e0c040718e43ff1b91669866fea4e325ce42656decb2bcb96'
